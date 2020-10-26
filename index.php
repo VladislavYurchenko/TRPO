579bab8efdaad64296d2b22685184a953ab7a97b
VladislavYurchenko <59086101+VladislavYurchenko@users.noreply.github.com>
2020-10-26 13:12:02 +0300

    2 lab

--- index.php
+++ index.php
@@ -1,44 +1,48 @@
 <?php 
 class A
-{}
+{
+    protected $x;   
+    function equation($a,$b){
+        if($a==0){
+            return false;
+        }
+        $x = -$b/$a;
+        $this->x = $x;
+        return $x;
+    }
+}
 class B extends A 
 {
-    public function __construct($a)
-    {
-        $this->a = $a;
+    function equation2($a,$b,$c){
+        $d = $this->discriminant($a,$b,$c); 
+        if($a==0){
+            return $this->x = $this->equation($b,$c);
+            //eturn $this->equation($b,$c);
+        }
+        if ($d<0){
+            $this->x = false;
+            return false;
+        }
+        $d = sqrt($d);
+        $x1 = ((-$b) + $d) / (2 * $a);
+        $x2 = ((-$b) - $d) / (2 * $a);
+        if($x1 == $x2){
+            $this->x = $x1;
+            return $x1;
+        }else{
+            $this->x = array($x1,$x2);
+            return  array($x1,$x2);
+        }
     }
-    protected $a;   
-}
-class C extends B
-{
-    public function __construct($a,$b,$c)
-    {
-        parent::__construct($a);
-        $this->b = $b;
-        $this->c = $c;  
-    }
-    protected $b;
-    protected $c;
+    protected function discriminant($a,$b,$c){
+        $d =  $b * $b - 4 * $a * $c;
+        return $d;
+    }   
 }
 
+$a = new A();
+$b = new B();
 
-$a1 = new A();
-$a2 = new A();
-
-
-$b1 = new B($a1);
-$b2 = new B($b1);
-
-$c = new C($a1,$a2,$b2);
-
-echo '<pre>';
-var_dump($c);
-echo '</pre>';
-$c = new C(new A(),new A(),new B(new B(new A())));
-
-
-//var_dump($c);
-
-
-
+//var_dump($b)
+ var_dump($b->equation2(1,4,3)); 
 ?>
